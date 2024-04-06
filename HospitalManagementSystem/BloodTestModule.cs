b7809ea47f2811d830169ed60f880cbb862da087
Yusif Yusifli <yusifry@code.edu.az>
2024-04-07 02:49:00 +0400

    last

---
+++ HospitalManagementSystem/BloodTestModule.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem
+{
+    public class BloodTestModule
+    {
+        public static Dictionary<string, int> GetBloodTest()
+        {
+            Random random = new Random();
+            Dictionary<string, int> bloodTest = new Dictionary<string, int>();
+            bloodTest.Add("Leykosit", random.Next(1, 101));
+            bloodTest.Add("Eritrosit", random.Next(1, 101));
+            bloodTest.Add("Kreatinin", random.Next(1, 101));
+            return bloodTest;
+        }
+        public static void DoBloodTest(Dictionary<string, int> bloodTest, string patientName)
+        {
+            Console.WriteLine("Qan tehlili neticeleri:");
+            foreach (var item in bloodTest)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            foreach (var item in bloodTest)
+            {
+                if (item.Value >= 70)
+                {
+                    string Report = $"Xeste - {patientName}-da {item.Key} uzre tehlil neticesi normadan artiqdir !!! Deger - {item.Value} ";
+                    SendAlert(Report);
+                }
+            }
+        }
+
+        private static void SendAlert(String message)
+        {
+            ViaEmail(message);
+
+            ViaSMS(message);
+
+            ViaTelegram(message);
+        }
+
+        private static void ViaEmail(string Report)
+        {
+            Console.WriteLine($"E-mail gönderildi - {Report}");
+        }
+
+        private static void ViaSMS(string Report)
+        {
+            Console.WriteLine($"SMS gönderildi -  {Report}");
+        }
+        private static void ViaTelegram(string Report)
+        {
+            Console.WriteLine($"Telegram gönderildi - {Report}");
+        }
+    }
+}
---
+++ HospitalManagementSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace HospitalManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Xestenin tam adini daxil edin:");
+                string patientFullName = Console.ReadLine();
+                Dictionary<string, int> kanTahlili = BloodTestModule.GetBloodTest();
+
+                BloodTestModule.DoBloodTest(kanTahlili,patientFullName);
+
+            }
+        }
+    }
+}
